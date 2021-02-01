eb714a557f51502e7d174e979a7d97b90898c6d8
Pranavan-1803 <76735704+Pranavan-1803@users.noreply.github.com>
2021-02-01 20:31:03 +0530

    first

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World...')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
+app.get('/login', (req, res) => {
+    res.send('you entered login page')
+  })
+
+  app.get('/signup', (req, res) => {
+    res.send('you entered signup page')
+  })
